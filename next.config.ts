c96ba29bcec47c8f7a9bf1c424f5bfdad6172183
Matheus Moraes <matheusmoraesdj2025@gmail.com>
2026-08-13 22:55:56 -0300

    feat: enable headers required by sqlite opfs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  headers: async () => [
+    {
+      source: "/(.*)",
+      headers: [
+        { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+        { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
